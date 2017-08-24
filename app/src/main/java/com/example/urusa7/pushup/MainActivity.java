0c3028ce33a6c7b3adcf562df9da7b1a34c23320
urusa7 <abt737@gmail.com>
2017-08-24 00:15:43 -0700

    Modified MainActivity Added New File "TestAddFile.java"

--- app/src/main/java/com/example/urusa7/pushup/MainActivity.java
+++ app/src/main/java/com/example/urusa7/pushup/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
         int i = 1+1;
 
+        int ii = 2+2;
+
+        int iii = i+ii;
+
     }
 
 
---
+++ app/src/main/java/com/example/urusa7/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.example.urusa7.pushup;
+
+/**
+ * Created by urusa7 on 2017-08-24.
+ */
+
+public class TestAddFile {
+}
